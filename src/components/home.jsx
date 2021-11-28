ae31528281d5a13103dbe8d84fed52f2bf88ab2a
jisoo <devjisoolee@gmail.com>
2021-11-28 23:49:20 +0900

    소개 문구 추가

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 const Wrapper = styled.div`
-  /* height: 100%; */
+  ${({ theme }) => {
+    return css`
+      background-color: ${theme.colors.black};
+      /* background-color: black; */
+      color: ${theme.colors.silver};
+      /* font-size: ${theme.fonts.size.base}; */
+    `;
+  }}
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledH1 = styled.h1`
+  margin: 0;
+  padding: 0.2rem;
 `;
 
 const Image = styled.img`
-  width: 70rem;
+  width: 40rem;
+  height: 40rem;
+  border-radius: 50%;
 `;
 
 const Home = props => {
   return (
     <Wrapper>
+      <StyledH1>
+        안녕하세요! 매일 한뼘 씩 성장하는 개발자 이지수입니다.{' '}
+      </StyledH1>
       <Image src='/imgs/profile2.jpg'></Image>
-      <Image src='/imgs/profile-resize.jpg'></Image>
+      {/* <Image src='/imgs/profile-resize.jpg'></Image> */}
     </Wrapper>
   );
 };

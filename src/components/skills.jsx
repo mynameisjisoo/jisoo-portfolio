291a5efcb152a7e8fb3045d4900bc871a2e358dd
jisoo <devjisoolee@gmail.com>
2021-12-03 00:35:35 +0900

    Modify CSS : box's border-radius and paddings

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -15,13 +15,12 @@ const SkillsBox = styled.section`
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
-  /* width: 75rem; */
-
-  /* background-color: lightyellow; */
 `;
 
 const Skill = styled.article`
-  border: 3px solid wheat;
+  border: 3px solid ${({ theme }) => theme.darkThemeColors.pointColor};
+  border-radius: 1rem;
+  margin: 1rem;
   width: 25rem;
   padding: 1.5rem 1rem;
   display: flex;
@@ -30,14 +29,14 @@ const Skill = styled.article`
   & h3 {
     font-size: 1.5rem;
     text-align: center;
-    margin: 0.6rem 0;
+    margin: 0.6rem 0 1rem 0;
   }
+
   & li {
-    list-style: none;
     font-size: 1.2rem;
+    padding: 0 0.7rem;
   }
 `;
-
 const Icon = styled(FontAwesomeIcon)`
   font-size: 3rem;
   align-self: center;
@@ -50,65 +49,58 @@ const Icon = styled(FontAwesomeIcon)`
   }
 `;
 
-const Container = styled.div`
-  /* display: flex;
-  justify-content: center; */
-`;
-
 const Skills = props => {
   return (
     <Section>
-      <Title>Skills and Experience</Title>
-      <Container>
-        <SkillsBox>
-          <Skill>
-            <Icon icon={faHtml5} />
-            <h3>HTML</h3>
-            <li>
-              시멘틱 태그에 대해 이해하고 접근성을 고려하기 위해 노력합니다.
-            </li>
-          </Skill>
-          <Skill>
-            <Icon icon={faCss3} />
-            <h3>CSS</h3>
-            <li>mediaQuery를 사용한 반응형 스타일링</li>
-            <li>flex, grid 를 활용한 레이아웃</li>
-            <li>
-              Post CSS를 이용한 모듈화, styled-components를 이용한 컴포넌트
-              스타일링
-            </li>
-            <li>BEM 방법론 적용</li>
-          </Skill>
-          <Skill>
-            <Icon icon={faJs} />
-            <h3>JavaScript</h3>
-            <li>ES6 문법</li>
-            <li>Vanilla JS로 동적인 웹앱 개발 </li>
-            <li>Rest API 활용한 웹앱 개발</li>
-            <li>성능 최적화를 위한 브라우저 런타임 환경 이해</li>
-          </Skill>
-          <Skill>
-            <Icon icon={faReact} />
-            <h3>React</h3>
-            <li>React 기반의 SPA 개발 </li>
-            <li>React Hooks 사용, 함수 컴포넌트 개발</li>
-            <li>React Router, firebase 등 라이브러리 사용 경험</li>
-          </Skill>
-          <Skill>
-            <Icon icon={faJava} />
-            <h3>Java</h3>
-            <li></li>
-          </Skill>
-          <Skill>
-            <Icon icon={faTools} />
-            <h3>Tools</h3>
-            <li>Git, GitKraken</li>
-            <li>VS Code, Eclipse</li>
-            <li>Slack</li>
-            <li>Postman</li>
-          </Skill>
-        </SkillsBox>
-      </Container>
+      <Title>Skills</Title>
+      <SkillsBox>
+        <Skill>
+          <Icon icon={faHtml5} />
+          <h3>HTML</h3>
+          <li>
+            시멘틱 태그에 대해 이해하고 접근성을 고려하기 위해 노력합니다.
+          </li>
+        </Skill>
+        <Skill>
+          <Icon icon={faCss3} />
+          <h3>CSS</h3>
+          <li>mediaQuery를 사용한 반응형 스타일링</li>
+          <li>flex, grid 를 활용한 레이아웃</li>
+          <li>BEM 방법론 적용</li>
+          <li>
+            Post CSS를 이용한 모듈화, styled-components를 이용한 컴포넌트
+            스타일링
+          </li>
+        </Skill>
+        <Skill>
+          <Icon icon={faJs} />
+          <h3>JavaScript</h3>
+          <li>ES6 문법</li>
+          <li>Vanilla JS로 동적인 웹앱 개발 </li>
+          <li>Rest API 활용한 웹앱 개발</li>
+          <li>성능 최적화를 위한 브라우저 런타임 환경 이해</li>
+        </Skill>
+        <Skill>
+          <Icon icon={faReact} />
+          <h3>React</h3>
+          <li>React 기반의 SPA 개발 </li>
+          <li>React Hooks 사용, 함수 컴포넌트 개발</li>
+          <li>React Router, firebase 등 라이브러리 사용 경험</li>
+        </Skill>
+        <Skill>
+          <Icon icon={faJava} />
+          <h3>Java</h3>
+          <li></li>
+        </Skill>
+        <Skill>
+          <Icon icon={faTools} />
+          <h3>Tools</h3>
+          <li>Git, GitKraken</li>
+          <li>VS Code, Eclipse</li>
+          <li>Slack</li>
+          <li>Postman</li>
+        </Skill>
+      </SkillsBox>
     </Section>
   );
 };
